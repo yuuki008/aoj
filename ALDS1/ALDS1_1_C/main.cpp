495e67c77c0ec1e3d5ead67862fd45ed37ed8dd4
yuuki008 <s22ah261@gmail.com>
2024-06-07 18:38:27 +0900

    インデント

--- ALDS1/ALDS1_1_C/main.cpp
+++ ALDS1/ALDS1_1_C/main.cpp
@@ -13,7 +13,7 @@ bool isPrime(int x) {
     if (x % 2 == 0 || x % 3 == 0) return false; 
 
     for (int i = 5; i * i <= x; i += 6) {
-        if (x % i == 0 || x % (i + 2) == 0) return false;
+      if (x % i == 0 || x % (i + 2) == 0) return false;
     }
     return true;
 }
