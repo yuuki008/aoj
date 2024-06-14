274ed766c2e5242ac814147c0a3849c0bbf650ef
yuuki008 <s22ah261@gmail.com>
2024-06-15 00:40:44 +0900

    CountingSort

--- ALDS1/ALDS1_6_A/main.cpp
+++ ALDS1/ALDS1_6_A/main.cpp
@@ -6,54 +6,50 @@
 #include <cmath>
 #include <iomanip>
 #include <climits>
+
 using namespace std;
 
-vector<int> countingSort(vector<int>& arr) {
-    int size = arr.size();
-    if (size == 0) return arr; // 空配列の処理
+vector<int> countingSort(vector<int> arr) {
+  int n = arr.size();
+  int min = INT_MAX;
+  int max = INT_MIN;
 
-    // 最大値と最小値を見つける
-    int max_val = *max_element(arr.begin(), arr.end());
-    int min_val = *min_element(arr.begin(), arr.end());
+  for(int i=0; i<n; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+  };
+  int range = max - min + 1;
 
-    // カウント配列のサイズを決定
-    int range = max_val - min_val + 1;
-    vector<int> count(range, 0);
-    vector<int> output(size, 0);
+  vector<int> count(range, 0);
+  vector<int> output(n);
 
-    // 出現回数をカウント
-    for (int num : arr) {
-        count[num - min_val]++;
-    }
+  for(int i=0; i<n; i++) count[arr[i]-min]++;
 
-    // 累積和を計算
-    for (int i = 1; i < range; i++) {
-        count[i] += count[i - 1];
-    }
+  for(int i=1; i<range; i++) count[i] += count[i-1];
 
-    // 出力配列に値を配置
-    for (int i = size - 1; i >= 0; i--) {
-        int current_val = arr[i];
-        int output_index = count[current_val - min_val] - 1;
-        output[output_index] = current_val;
-        count[current_val - min_val]--;
-    }
+  for(int i=0; i<n; i++) {
+    int out_i = count[arr[i]-min]-1;
+    output[out_i] = arr[i];
+    count[arr[i]-min]--;
+  }
 
-    return output;
-}
+  return output;
+};
 
 
 int main() {
   int n;
   cin >> n;
   vector<int> A(n);
-  for(int i=0; i<n; i++) cin >> A[i];
+  for (int i=0; i<n; i++) cin >> A[i];
 
-  vector<int> sorted = countingSort(A);
-  for (int i=0; i<n; i++) {
+  vector<int>ans = countingSort(A);
+
+  for(int i=0; i<n; i++) {
     if (i != 0) cout << " ";
-    cout << sorted[i];
+    cout << ans[i];
   };
+
   cout << endl;
 
   return 0;
