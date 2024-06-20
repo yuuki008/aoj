5cc2434bf6c60b2f59765e20b125f7e9c2611f02
yuuki008 <s22ah261@gmail.com>
2024-06-20 21:29:12 +0900

    リファクタ

--- ALDS1/ALDS1_7_A/main.cpp
+++ ALDS1/ALDS1_7_A/main.cpp
@@ -12,81 +12,70 @@ class Node {
     vector<int> children;
 
     Node(int id) : id(id), parent(-1), depth(0) {}
-    string type() {
+
+    string type() const {
       if (parent == -1) return "root";
-      else if (children.size() == 0) return "leaf";
+      else if (children.empty()) return "leaf";
       return "internal node";
     }
 };
 
 void setDepth(int id, vector<Node>& nodes, int depth) {
   nodes[id].depth = depth;
-
-  int size = nodes[id].children.size();
-
-  for(int i=0; i<size; i++) {
-    Node child = nodes[id].children[i];
-    setDepth(child.id, nodes, depth+1);
-  };
-};
+  for (int childId : nodes[id].children) {
+    setDepth(childId, nodes, depth + 1);
+  }
+}
 
 int main() {
   int n;
   cin >> n;
 
-  vector<vector<int>>in;
-  vector<Node> nodes;
-
-  for(int i=0; i<n; i++) {
+  vector<Node> nodes(n);
+  for (int i = 0; i < n; ++i) {
     int id, k;
     cin >> id >> k;
-
-    Node node = Node(id);
-    for (int j=0; j<k; j++) {
+    nodes[id] = Node(id);
+    for (int j = 0; j < k; ++j) {
       int c;
       cin >> c;
-      node.children.push_back(c);
+      nodes[id].children.push_back(c);
     }
-    nodes.push_back(node);
-  };
+  }
 
-  // node の id 番目に node をセット
-  for(int i=0; i<n; i++) swap(nodes[i], nodes[nodes[i].id]);
 
-  // parent のセット
-  for(int i=0; i<n; i++) {
-    Node parent = nodes[i];
-    vector<int> children = nodes[i].children;
-
-    for(int j=0; j<children.size(); j++) {
-      int childId = children[j];
-      nodes[childId].parent = parent.id;
+  // parent の設定
+  for (auto& node : nodes) {
+    for (int childId : node.children) {
+      nodes[childId].parent = node.id;
     }
   }
 
   // root を見つける
   int rootId = 0;
-  for(const auto node : nodes) {
-    if(node.parent == -1) {
+  for (const auto& node : nodes) {
+    if (node.parent == -1) {
       rootId = node.id;
       break;
     }
   }
 
+  // 深さをセット
   setDepth(rootId, nodes, 0);
 
-  for(int i=0; i<n; i++) {
-    cout << "node " << nodes[i].id << ": ";
-    cout << "parent = " << nodes[i].parent << ", ";
-    cout << "depth = " << nodes[i].depth << ", ";
-    cout << nodes[i].type() << ", ";
 
-    vector<int> children = nodes[i].children;
+  // 出力
+  for (const auto& node : nodes) {
+    cout << "node " << node.id << ": ";
+    cout << "parent = " << node.parent << ", ";
+    cout << "depth = " << node.depth << ", ";
+    cout << node.type() << ", ";
+
     cout << "[";
-    for (int j=0; j<children.size(); j++) {
-      cout << children[j];
-      if (j<children.size()-1) cout << ", ";
-    };
+    for (size_t j = 0; j < node.children.size(); ++j) {
+      cout << node.children[j];
+      if (j < node.children.size() - 1) cout << ", ";
+    }
     cout << "]" << endl;
   }
 
